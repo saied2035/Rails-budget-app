391a2ca10a199ec2b3c07446a928a8119ea7eb81
Saied Gaffer <saied2421998@gmail.com>
2022-07-06 09:51:09 +0200

    finish login / sign up style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,17 @@ class ApplicationController < ActionController::Base
     
     protected
 
+    def after_sign_up_path_for(_resource_or_scope)
+      catagories_url
+    end
+
+    def after_sign_in_path_for(_resource_or_scope)
+      catagories_url
+    end
+
+    def after_sign_out_path_for(_resource_or_scope)
+      root_url
+    end
     def update_allowed_parameters
       devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
       devise_parameter_sanitizer.permit(:account_update) do |u|
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers:{registrations:"devise/registrations"}
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :catagories, only:[:index,:show,:splash_screen]
   # Defines the root path route ("/")
