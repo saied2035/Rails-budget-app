120c9bf6cecbb6b6560cc6132d4813335a586138
Saied Gaffer <saied2421998@gmail.com>
2022-11-07 18:53:35 +0200

    add host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
-
+  config.hosts << "https://rails-budget-app-production.up.railway.app"
   # Enable server timing
   config.server_timing = true
 
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema[7.0].define(version: 2022_07_04_152711) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "timescaledb"
 
   create_table "catagories", force: :cascade do |t|
     t.bigint "author_id"
