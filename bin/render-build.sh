54f40176f9f2cccd78c3afc8aa0894f15b77d4f8
saied2035 <saied1998@gmail.com>
2022-11-26 17:05:33 +0200

    try to deploy for render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,9 +2,6 @@
 # exit on error
 set -o errexit
 
-export RUBY_VERSION=$(cat $RENDER_SRC_ROOT/web/.ruby-version)
-export BUNDLE_GEMFILE=$RENDER_SRC_ROOT/web/Gemfile
-set_ruby_env $(fetch_or_build "$RUBY_VERSION")
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
