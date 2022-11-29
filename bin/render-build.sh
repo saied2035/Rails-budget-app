b83e74fe75783abcc7076e0c1edc3d39854cbe33
saied2035 <saied1998@gmail.com>
2022-11-29 15:04:33 +0200

    fix build script 2

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 export RUBY_VERSION=3.1.2 && 
-export BUNDLE_GEMFILE=$RENDER_SRC_ROOT/budget-app/Gemfile &&
+export BUNDLE_GEMFILE=$RENDER_SRC_ROOTGemfile &&
 set_ruby_env $(fetch_or_build "3.1.2") &&
 /opt/render/project/rubies/ruby-3.1.2/bin/gem update --system &&
 /opt/render/project/rubies/ruby-3.1.2/bin/bundle config set --local deployment 'true'
