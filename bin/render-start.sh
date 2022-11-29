37c542254b389b3b46ee617093fc00af40f6e9f7
saied2035 <saied1998@gmail.com>
2022-11-29 10:48:32 +0200

    try to modify build and start sh files to deploy to render

--- bin/render-start.sh
+++ bin/render-start.sh
@@ -2,8 +2,4 @@
 # exit on error
 set -o errexit
 
-export RUBY_VERSION=$(cat $RENDER_SRC_ROOT .ruby-version)
-export BUNDLE_GEMFILE=$RENDER_SRC_ROOT Gemfile
-source /home/render/ruby-env.sh
-set_ruby_env "$RUBIES_ROOT/ruby-$RUBY_VERSION"
 bundle exec puma -C ./config/puma.rb
