69a588d5ea24344af241e112cf21f9258af76345
saied2035 <saied1998@gmail.com>
2022-11-29 15:10:36 +0200

    fix build script 3

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -8,5 +8,6 @@ set_ruby_env $(fetch_or_build "3.1.2") &&
 /opt/render/project/rubies/ruby-3.1.2/bin/gem update --system &&
 /opt/render/project/rubies/ruby-3.1.2/bin/bundle config set --local deployment 'true'
 /opt/render/project/rubies/ruby-3.1.2/bin/bundle install &&
-bin/rails assets:precompile &&
-bin/rails assets:clean
+bundle exec rake assets:precompile &&
+bundle exec rake assets:clean &&
+bundle exec rake db:migrate
