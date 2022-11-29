0b87019a43aee1f935dd8e16ee62c731edbc691e
saied2035 <saied1998@gmail.com>
2022-11-29 17:21:02 +0200

    try to change the build comaand

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,18 +2,12 @@
 # exit on error
 set -o errexit
 
-echo "start path" &&
-
-echo $PATH &&
-
-echo "end path" &&
-
-export RUBY_VERSION=3.1.2 && 
-export BUNDLE_GEMFILE=$RENDER_SRC_ROOTGemfile &&
-set_ruby_env $(fetch_or_build "3.1.2") &&
-/opt/render/project/rubies/ruby-3.1.2/bin/gem update --system &&
+export RUBY_VERSION=3.1.2
+export BUNDLE_GEMFILE=$RENDER_SRC_ROOTGemfile
+set_ruby_env $(fetch_or_build "3.1.2")
+/opt/render/project/rubies/ruby-3.1.2/bin/gem update --system
 /opt/render/project/rubies/ruby-3.1.2/bin/bundle config set --local deployment 'true'
-/opt/render/project/rubies/ruby-3.1.2/bin/bundle install &&
-bundle exec rake assets:precompile &&
-bundle exec rake assets:clean &&
+/opt/render/project/rubies/ruby-3.1.2/bin/bundle install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
 bundle exec rake db:migrate
