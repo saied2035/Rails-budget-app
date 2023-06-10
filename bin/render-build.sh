019ff8cff1355205853fea28701de3f2017a5180
saied2035 <saied1998@gmail.com>
2023-06-10 20:56:21 +0300

    Configure the project for cockroachdb & render for deployment

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,4 +5,4 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle curl --create-dirs -o $HOME/.postgresql/root.crt <%= ENV['CONNECT_DATABASE'] %>
+bundle curl --create-dirs -o $HOME/.postgresql/root.crt ENV['CONNECT_DATABASE']
