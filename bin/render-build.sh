58addbbaf4e2bcd430a8ea1b934f676d7d0be2e2
saied2035 <saied1998@gmail.com>
2023-06-10 20:53:31 +0300

    Configure the project for cockroachdb & render for deployment

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,4 +5,4 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle curl --create-dirs -o $HOME/.postgresql/root.crt 'https://cockroachlabs.cloud/clusters/624ae424-1b9b-4c83-b25f-90ea899c70dd/cert'
+bundle curl --create-dirs -o $HOME/.postgresql/root.crt <%= ENV['DATABASE_URL'] %>
